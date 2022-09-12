121a8c155af58bc8bc441c85fa1b61039d5e89e4
Glitchy <thepatheticweebgamer@gmail.com>
2022-09-12 17:02:51 -0400

    :recycle: Changed database logging plus errors
    Added database file error handling and changed logging to debug

--- core/database.py
+++ core/database.py
@@ -6,7 +6,6 @@ from sqlitedict import SqliteDict
 db_file = f"{Path(__file__).parent.parent.absolute()}/cache.sqlite3"
 
 
-@log.catch()
 def set_confess_channel(guild_id: int, channel_id: int) -> None:
     """Function to set the confession channel for a given guild
 
@@ -15,17 +14,22 @@ def set_confess_channel(guild_id: int, channel_id: int) -> None:
     :return: None
     """
     with SqliteDict(db_file, outer_stack=False, autocommit=True) as db:
-        db[guild_id] = {"confess channel": channel_id}
-        log.success(f"Successfully set confession channel for {guild_id}")
+        try:
+            db[guild_id] = {"confess channel": channel_id}
+            log.debug(f"Successfully set confession channel for {guild_id}")
+        except Exception:
+            log.opt(exception=True).exception("An error occurred when trying to set guild confess channel")
 
 
-@log.catch()
-def get_confess_channel(guild_id: int) -> int:
+def get_confess_channel(guild_id: int) -> None:
     """Function to fetch the confession channel for a given guild
 
     :param guild_id: The id of the guild
     :return: The channels id
     """
     with SqliteDict(db_file, outer_stack=False) as db:
-        confess_channel = db[guild_id]["confess channel"]
-        return confess_channel
+        try:
+            confess_channel = db[guild_id]["confess channel"]
+            return confess_channel
+        except KeyError:
+            KeyError("Guild has not set a confession channel")
