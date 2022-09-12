acec7f21a6ce5e07a33342c41bf3ed027a028c64
Glitchy <thepatheticweebgamer@gmail.com>
2022-09-12 17:21:11 -0400

    :ambulance: Fixed debug issue
    Fixed Checking if Debug is enabled or not

--- __main__.py
+++ __main__.py
@@ -1,4 +1,5 @@
 import os
+from distutils.util import strtobool
 
 from dotenv import load_dotenv
 from naff import Intents
@@ -9,9 +10,9 @@ if __name__ == "__main__":
 
     # Load environment vars
     load_dotenv()
-    log.level("DEBUG" if os.getenv("DEBUG") else "INFO")
+    log.level("DEBUG" if strtobool(os.getenv("DEBUG")) else "INFO")
 
-    if os.getenv("DEBUG"):
+    if strtobool(os.getenv("DEBUG")):
         import jurigged
 
         jurigged.watch(logger=jlogger)
@@ -20,4 +21,4 @@ if __name__ == "__main__":
     bot = Megumin(intents=Intents.ALL, auto_defer=True)
 
     # Start the bot
-    bot.start(os.getenv("DEV_DISCORD_TOKEN") if os.getenv("DEBUG") else os.getenv("DISCORD_TOKEN"))
+    bot.start(os.getenv("DEV_DISCORD_TOKEN") if strtobool(os.getenv("DEBUG")) else os.getenv("DISCORD_TOKEN"))
