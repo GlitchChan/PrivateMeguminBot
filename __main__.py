61249f5a29b88cf24835172c3a4c6ba4a998cc09
Glitchy <thepatheticweebgamer@gmail.com>
2022-09-12 16:52:37 -0400

    :sparkles: Added jurigged
    Added jurigged for faster prototyping

--- __main__.py
+++ __main__.py
@@ -1,28 +1,23 @@
 import os
 
 from dotenv import load_dotenv
-from loguru import logger as log
 from naff import Intents
 
-from core import Megumin
+from core import Megumin, jlogger, log
 
 if __name__ == "__main__":
 
     # Load environment vars
     load_dotenv()
-
-    # Init Logging
-    log.add(
-        "logs/megumin.log",
-        backtrace=True,
-        enqueue=True,
-        diagnose=True,
-        rotation="12:00",
-        retention="10 days",
-    )
     log.level("DEBUG" if os.getenv("DEBUG") else "INFO")
+
+    if os.getenv("DEBUG"):
+        import jurigged
+
+        jurigged.watch(logger=jlogger)
 
     # Create bot instance
     bot = Megumin(intents=Intents.ALL, auto_defer=True)
 
-    bot.start(os.getenv("DISCORD_TOKEN"))
+    # Start the bot
+    bot.start(os.getenv("DEV_DISCORD_TOKEN") if os.getenv("DEBUG") else os.getenv("DISCORD_TOKEN"))
--- core/__init__.py
+++ core/__init__.py
@@ -1,4 +1,5 @@
 from .checks import has_permission
 from .client import Megumin
+from .custom_logger import *
 from .database import get_confess_channel, set_confess_channel
 from .embedbuilder import confession_embed, embed_builder
