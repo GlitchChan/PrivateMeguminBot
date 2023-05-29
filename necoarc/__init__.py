a4de48a5c32f65929db45bd53771b794a2566039
glitchy <glitchy@cunny.social>
2023-05-29 19:58:19 -0400

    :sparkles: Added funny twitter feature and updated database

--- necoarc/__init__.py
+++ necoarc/__init__.py
@@ -1,11 +1,11 @@
 from .checks import has_permission, is_trusted
 from .database import (
+    get_cnuy_channel,
     get_confession_channel,
-    get_log_channel,
     get_sex_leaderboard,
+    set_cnuy_channel,
     set_confession_channel,
     set_user_sex_count,
-    toggle_confessions,
     update_user_sex_count,
 )
 
@@ -14,9 +14,9 @@ __all__ = (
     "is_trusted",
     "get_confession_channel",
     "get_sex_leaderboard",
-    "get_log_channel",
     "set_confession_channel",
     "set_user_sex_count",
-    "toggle_confessions",
     "update_user_sex_count",
+    "get_cnuy_channel",
+    "set_cnuy_channel",
 )
